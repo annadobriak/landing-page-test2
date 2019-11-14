b0c98b4039ffd149059db0ec152cc32d8e251b49
Dobriak Anna <dobriakanna@gmail.com>
2019-11-14 14:11:34 +0200

    adding files

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+$('.slider__inner').slick({
+	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+});
+
+$('.slider__info-inner').slick({
+	prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+	nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+});
+
+});
